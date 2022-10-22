2b63c7958b8ebba5e28bcf09fc1031c49cacd645
adaloui <adnen.aloui2@hotmail.com>
2022-10-23 01:45:06 +0200

    Inception -- MariaDB fonctionnel, reste les ALTER USER et var env

--- srcs/requirements/mariadb/conf/script_db_creation.sql
+++ srcs/requirements/mariadb/conf/script_db_creation.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE IF NOT EXISTS Adnen_base;
+/*CREATE DATABASE IF NOT EXISTS Adnen_base;
 CREATE USER IF NOT EXISTS 'Sasuke'@'%' IDENTIFIED BY 'popo';
-GRANT ALL PRIVILEGES ON Adnen_base.* TO 'Sasuke'@'%';
+GRANT ALL PRIVILEGES ON Adnen_base.* TO 'Sasuke'@'%';*/
 /*FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpopo';
 */
@@ -10,3 +10,11 @@ GRANT ALL PRIVILEGES ON Base.* TO 'bca'@'%';
 FLUSH PRIVILEGES;
 
 ALTER USER 'root'@'localhost' IDENTIFIED BY '1qaz2wsx';*/
+
+
+
+CREATE DATABASE IF NOT EXISTS wordpress;
+CREATE USER IF NOT EXISTS 'adaloui'@'%' IDENTIFIED BY 'popo';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'adaloui'@'%';
+FLUSH PRIVILEGES;
+/*ALTER USER 'root'@'localhost' IDENTIFIED BY 'rootpopo';*/
