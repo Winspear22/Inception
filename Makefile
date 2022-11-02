0dd0dac3d92c3bb45cff726bf6680aac7a611216
user42 <adnen.aloui2@hotmail.com>
2022-11-02 18:29:35 +0100

    Inception -- Marche, mais aucune idee pourquoi --> investiguer

---
+++ Makefile
@@ -0,0 +1,51 @@
+
+all: up
+
+up :	
+	cd srcs && sudo service docker restart \
+	&& sudo docker-compose -f docker-compose.yml build \
+	&& sudo docker-compose -f docker-compose.yml up -d
+
+down :
+	cd srcs && sudo docker-compose -f docker-compose.yml down
+
+clean:	
+	cd srcs && sudo docker-compose -f docker-compose.yml down \
+	&& sudo docker system prune -a --force \
+	&& sudo docker volume rm srcs_mariadb-volume srcs_wp-files-volume
+
+re:	clean up
+
+show:
+	sudo docker container ps -a
+
+nginx:
+	sudo docker exec -it nginx bash -l
+
+mariadb:
+	sudo docker exec -it mariadb bash -l
+
+wordpress:
+	sudo docker exec -it wordpress bash -l
+
+config:
+	cd srcs && sudo docker-compose config
+delete:
+	cd srcs && sudo docker-compose down --remove-orphans --volumes && sudo docker system prune --force
+
+volume_show:
+	cd /home/adaloui/data && sudo ls -l **
+
+volume_delete:
+	sudo rm -Rf /home/adaloui/data/*
+
+up_nginx:
+	cd srcs && sudo docker-compose up --build nginx
+up_mariadb:
+	cd srcs && sudo docker-compose up --build mariadb
+up_wordpress:
+	cd srcs && sudo docker-compose up --build wordpress
+
+
+.PHONY: up down re clean show nginx mariadb wordpress config delete volume_show volume_delete \
+up_nginx up_mariadb up_wordpress
--- srcs/requirements/wordpress/conf/script.sh
+++ srcs/requirements/wordpress/conf/script.sh
@@ -1,32 +1,65 @@
 #!/bin/bash
 
-# JE LAISSE LE TEMPS A MYSQL DE SE LANCER
-sleep 10
+sleep 5
 
-# ETAPE TRES IMPORTANTE, J'ORDONNE AU SHELL DE SE RENDRE DANS MON DOSSIER WORDPRESS
-# IL FAUT QU'ON SOIT DANS LE DOSSIER OU SE TROUVE LE WP-CONFIG ET LE WP-SETTINGS SINON, TOUTES MES COMMANDES WP NE MARCHERONT PAS
+mkdir -p /var/www/html/wordpress
+chmod 777 /var/www/html
+chown -R www-data:www-data /var/www/html/
 cd /var/www/html/wordpress/
 
-# JE DEMANDE SI WP-CONFIG EST CREE, SI NON JE LE CREE SPECIFIQUEMENT DANS LE DOSSIER WORDPRESS
-# AVEC LES CRITERES DEMANDES
+
+
+# ETAPE 1: 
+# - Installation de l'interface en ligne de commande de wordpress (wp-cli) afin d'intaller et configurer wordpress facilement via le ce script bash
+
+wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+mv wp-cli.phar /usr/local/bin/wp
+
+
+
+# ETAPE 2 :
+# - Téléchargement de wordpress
+
+wp core download --allow-root --locale=fr_FR
+
+
+
+# ETAPE 3 :
+# - Creation du fichier de configuration wp-config.php afin de configurer automatiquement wordpress (demandé dans le sujet)
+
 if [ ! -f /var/www/html/wordpress/wp-config.php ];
 then
-    wp config create --allow-root --dbname=wordpress --dbuser=adaloui --dbpass=popo --dbhost=mariadb:3306 --path='/var/www/html/wordpress'
+	wp config create --allow-root --dbname=${MARIADB_DATABASE} --dbuser=${MARIADB_USER} --dbpass=${MARIADB_PASSWORD} --dbhost=mariadb:3306 --path='/var/www/html/wordpress'
 fi
-# JE SUPPRIME LE FICHIER EXEMPLE, CAR WORDPRESS CONFOND LE NOTRE ET L'EXEMPLE
-rm -Rf wp-config-sample.php
-# CONFIGURATION AUTMATIQUE DU SITE
-wp core install --allow-root --url=https://adaloui.42.fr --title="Inception" --admin_user=adaloui --admin_password=popo --admin_email=adaloui@student.42.fr
-# CREATION DE NOTRE DEUXIEME UTILISATEUR CAR C'EST DEMANDE DANS LE SUJET
-wp user create --allow-root user user@user.42.fr --role=author --user_pass=user
-# J'INSTALLE MON THEME : J'AI CHOISU LE TWENTYSEVENTEEN
-wp theme install twentyseventeen --activate --allow-root
-# JE VALIDE TOUTES MES MODIFICATIONS AVEC UN FLUSH
+
+
+
+# ETAPE 4 :
+# - Configurer les info du site automatiquement (titre du site + info de l'user admin de wp)
+
+wp core install --allow-root --url=https://adaloui.42.fr --title="Inception" --admin_user=${WORDPRESS_ADMIN_USER} --admin_password=${WORDPRESS_ADMIN_PASSWORD} --admin_email=${WORDPRESS_ADMIN_EMAIL}
+
+
+
+# ETAPE 5 :
+# Creer un second user pour wordpress (demandé dans le sujet)
+
+wp user create --allow-root ${WORDPRESS_USER_2} ${WORDPRESS_USER_2_EMAIL} --role=author --user_pass=${WORDPRESS_USER_2_MDP}
+
+
+
+# ETAPE 6 :
+# Personnalisation
+
+wp theme install wpkites --activate --allow-root
+#wp post delete 1 --allow-root
 wp cache flush --allow-root
-# JE LANCE PHP-FPM AFIN QU'IL PUISSE TOURNER EN PARALLELE DE MON WORDPRESS ET COMMUNIQUER AVEC NGINX COMME DEMANDE DANS LE SUJET
-if [ ! -f /run/php ];
-then
-    mkdir /run/php
-fi
-echo "Wordpress est prêt"
+
+# ETAPE 7 :
+# - Lancer php-fpm en mode Foreground pour le lancer en premier plan : Comme ça le conteneur docker restera actif et ne s'arretera pas tant que la CMD ne retourne pas de code de sortie.
+# PHP-FPM permet de faire fonctionner PHP de maniere separée : Ça permet la communication entre PHP qui est dans le conteneur wordpress et notre module FastCGI de notre server web (dans notre conteneur nginx)
+
+mkdir /run/php
+
 exec /usr/sbin/php-fpm7.3 -F -R
